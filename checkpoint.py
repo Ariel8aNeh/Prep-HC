760f1aa82d0709e13caae8e683d3735bc9590d15
unknown <ochoa.ariel1991@gmail.com>
2023-03-21 19:17:10 -0300

    checkpoint commit

--- checkpoint.py
+++ checkpoint.py
@@ -1,8 +1,9 @@
 # Importante: No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
 # código dentro de las funciones ya definidas.
 
+
 def Factorial(numero):
-    '''
+    """
     Esta función devuelve el factorial del número pasado como parámetro.
     En caso de que no sea de tipo entero y/o sea menor que 1, debe retornar nulo.
     Recibe un argumento:
@@ -10,13 +11,23 @@ def Factorial(numero):
     Ej:
         Factorial(4) debe retornar 24
         Factorial(-2) debe retornar nulo
-    '''
-    #Tu código aca:
-    return 'Funcion incompleta'
+    """
+    # Tu código aca:
+    if type(numero) != int:
+        return None
+    if numero < 1:
+        return None
+
+    resultado_factorial = 1
+    while numero > 1:
+        resultado_factorial = resultado_factorial * numero
+        numero = numero - 1
+    return resultado_factorial
+
 
 def EsPrimo(valor):
-    '''
-    Esta función devuelve el valor booleano True si el número reibido como parámetro es primo, de lo 
+    """
+    Esta función devuelve el valor booleano True si el número reibido como parámetro es primo, de lo
     contrario devuelve False..
     En caso de que el parámetro no sea de tipo entero debe retornar nulo.
     Recibe un argumento:
@@ -24,13 +35,24 @@ def EsPrimo(valor):
     Ej:
         EsPrimo(7) debe retornar True
         EsPrimo(8) debe retornar False
-    '''
-    #Tu código aca:
-    return 'Funcion incompleta'
-    
+    """
+    # Tu código aca:
+
+    if type(valor) != int:
+        return None
+
+    primo = True
+    for i in range(2, valor):
+        if valor % i == 0:
+            primo = False
+            break
+
+    return primo
+
+
 def ClaseAnimal(especie, color):
-    '''
-    Esta función devuelve un objeto instanciado de la clase Animal, 
+    """
+    Esta función devuelve un objeto instanciado de la clase Animal,
     la cual debe tener los siguientes atributos:
         Edad    (Un valor de tipo de dato entero, que debe inicializarse en cero)
         Especie (Un valor de tipo de dato string)
@@ -45,6 +67,16 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 1
         a.CumpliAnios() -> debe devolver 2
         a.CumpliAnios() -> debe devolver 3
-    '''
-    #Tu código aca:
-    return 'Funcion incompleta'
+    """
+    # Tu código aca:
+    class Animal:
+        def __init__(self, especie, color):
+            self.edad = 0
+            self.especie = especie
+            self.color = color
+
+        def CumplirAnios(self):
+            self.edad = self.edad + 1
+            return self.edad
+
+    return Animal(especie, color)
